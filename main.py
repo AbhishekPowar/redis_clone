e49706ddec40c32e253854a1514b6d244f7249e0
Abhishek Powar <mailabhipowar@gmail.com>
2022-02-15 23:41:17 +0530

    Removed flask.sh and main.py from project

--- main.py
+++
@@ -1,8 +0,0 @@
-from redis_client import Redis_client
-
-if __name__ == "__main__":
-    redis = Redis_client(host='http://localhost', port=5000, index='index')
-    redis.set(key='name', value='Abhishek')
-    redis.get(key='name')
-    redis.remove(key='name')
-    redis.get(key='name')
